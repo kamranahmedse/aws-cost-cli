89863448d146095c742a4cc979a84e9e14158d50
Akira Hayashi <smxshxishxad@me.com>
2024-05-31 06:21:54 +0900

    Transpile dependent packages under `node_modules/`
    to avoid error below:
    > SyntaxError: Cannot use import statement outside a module

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,3 +1,16 @@
+var esmModules = [
+  "chalk",
+  "ora",
+  "cli-cursor",
+  "restore-cursor",
+  "log-symbols",
+  "is-unicode-supported",
+  "strip-ansi",
+  "ansi-regex",
+  "is-interactive",
+  "stdin-discarder",
+];
+
 module.exports = {
   preset: "ts-jest/presets/js-with-babel",
   testEnvironment: "node",
@@ -7,5 +20,8 @@ module.exports = {
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+  transformIgnorePatterns: [
+  `node_modules/(?!(?:.pnpm/)?(${esmModules.join('|')}))`,
+  ],
   testMatch: ["**/?(*.)+(test).[tj]s?(x)"],
 };
